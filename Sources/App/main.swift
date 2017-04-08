ed06509840fd3e3e4c4cf332d7d44871f691bc18
Mary <mlmartinez85@gmail.com>
2017-04-08 13:15:55 -0700

    Add leaf templating for projects

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -5,32 +5,36 @@ let drop = Droplet()
 try drop.addProvider(VaporPostgreSQL.Provider)
 drop.preparations += Project.self
 
-drop.get("version") { request in
-    if let db = drop.database?.driver as? PostgreSQLDriver {
-        let version = try db.raw("SELECT version()")
-        return try JSON(node: version)
-    } else {
-        return "No db connection"
-    }
-}
+let projectController = ProjectController()
+projectController.addRoutes(drop: drop)
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
-}
 
-drop.get("test") { request in
-    var project = Project(title: "HiveMine",
-                          description: "Blah",
-                          tech: "iOS",
-                          image: nil,
-                          video: nil,
-                          link: nil)
-    try project.save()
-    return try JSON(node: Project.all().makeNode())
-}
+//drop.get("version") { request in
+//    if let db = drop.database?.driver as? PostgreSQLDriver {
+//        let version = try db.raw("SELECT version()")
+//        return try JSON(node: version)
+//    } else {
+//        return "No db connection"
+//    }
+//}
+//
+//drop.get { req in
+//    return try drop.view.make("welcome", [
+//    	"message": drop.localization[req.lang, "welcome", "title"]
+//    ])
+//}
+//
+//drop.get("test") { request in
+//    var project = Project(title: "HiveMine",
+//                          description: "Blah",
+//                          tech: "iOS",
+//                          image: nil,
+//                          video: nil,
+//                          link: nil)
+//    try project.save()
+//    return try JSON(node: Project.all().makeNode())
+//}
 
-drop.resource("posts", PostController())
+//drop.resource("posts", PostController())
 
 drop.run()
